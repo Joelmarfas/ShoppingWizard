3009c740df7be8845ef0ef00ecaa60638f7eba25
Antonio Copete <antcopjim@gmail.com>
2021-07-07 13:52:58 +0200

    dates

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var radioButtons = document.querySelectorAll("[type='radio']");
 var mainContainerElements = document.querySelectorAll(".main-container > div");
 var carButton = document.getElementById("cart");
 
+var estimated = document.getElementById("estimated-time");
+
 giftCheckbox.addEventListener("click", giftArea);
 for (radioButton of radioButtons) {
   radioButton.addEventListener("click", shipmentDate);
@@ -21,11 +23,12 @@ function giftArea() {
 function giftArea() {
   var hiddenShippings = document.getElementsByClassName("flex-form");
   for (hiddenShipping of hiddenShippings) {
-    hiddenShipping.classList.toggle("hidden");
+    hiddenShipping.classList.toggle("invisible");
   }
 }
 
 function shipmentDate(e) {
+  estimated.innerHTML = "";
   var date = new Date();
   var nextDay = new Date();
 
@@ -42,7 +45,32 @@ function shipmentDate(e) {
       date.setDate(date.getDate() + 3);
       nextDay.setDate(date.getDate() + 1);
       break;
+    default:
+      console.log("ERROR");
   }
+
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var estimatedTime = `<h3>Your order will arrive:</h3>
+  <p> ESTIMATED DELIVERY DATE: </p>
+  <p> Between <b> ${date.getDate()} ${
+    monthNames[date.getMonth()]
+  } ${date.getFullYear()} </b> and <b> ${nextDay.getDate()} ${
+    monthNames[nextDay.getMonth()]
+  } ${nextDay.getFullYear()}</b>`;
+  estimated.insertAdjacentHTML("afterbegin", estimatedTime);
 }
 
 carButton.addEventListener("click", addToCart);
@@ -51,4 +79,8 @@ function addToCart() {
   for (mainContainerElement of mainContainerElements) {
     mainContainerElement.classList.add("hidden");
   }
+
+  var mainContainer = document.querySelector(".main-container");
+  var shipping = document.getElementById("shipping-container");
+  mainContainer.appendChild(shipping);
 }
